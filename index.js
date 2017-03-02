f3618fe99c470a189fc256438eec44626c2f05d7
klequis <klequis@gmail.com>
2017-03-02 11:06:26 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+// One
+// Traditional
+var t1 = function() {
+  return 't1-hi';
+}
+console.log('t1', t1());
+// Arrow
+
+
+
+// Two
+// Traditional
+var t2 = function(str) {
+  return str;
+}
+console.log('t2', t2('t2-hi'))
+// Arrow
+
+
+
+// Three
+var t3 = function(str1, str2) {
+  return str1 + ', ' + str2;
+}
+console.log('t3', t3('t3', 't3'))
+// Arrow
